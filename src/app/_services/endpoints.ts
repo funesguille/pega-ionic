102d74ec9fd3d5a4b7afb03ef6b4b78ac25509d7
funesguille <funesguille@users.noreply.github.com>
2020-06-01 11:29:00 -0400

    first commit

---
+++ src/app/_services/endpoints.ts
@@ -0,0 +1,14 @@
+export const endpoints = {
+  // Change this URL if you want to point the React application at another Pega server.
+  //BASEURL: "http://localhost:8080/prweb/api/v1",
+  BASEURL: "http://10.53.155.218/prweb/api/v1",
+  AUTH: "/authenticate",
+  CASES: "/cases",
+  CASETYPES: "/casetypes",
+  VIEWS: "/views",
+  ASSIGNMENTS: "/assignments",
+  ACTIONS: "/actions",
+  PAGES: "/pages",
+  DATA: "/data",
+  REFRESH: "/refresh"
+};
